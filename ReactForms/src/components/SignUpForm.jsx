c8b1be2ecfcd93b70e8b6b225f4c5d07ff564328
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-17 13:59:52 -0600

    placed async function to handle submit button and prevented refresh through event.preventDefault function on the handle function

--- ReactForms/src/components/SignUpForm.jsx
+++ ReactForms/src/components/SignUpForm.jsx
@@ -5,10 +5,16 @@ const SignUpForm = () => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("This is a Test!");
+  }
+
   return (
     <>
       <h2>Sign Up!</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Username:{" "}
           <input
