e26734cb07693540e7f3ec4cd99355ce4144396c
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-17 12:00:21 -0600

    imported useState to SignUpForm and created three state variables to input variables and set to empty string and null

--- ReactForms/src/components/SignUpForm.jsx
+++ ReactForms/src/components/SignUpForm.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
+
+const[]=useState();
+const[]=useState();
+const[]=useState();
 
 const SignUpForm = () => {
   return (
-    <div>Sign Up!</div>
+    <h2>Sign Up!</h2>
   )
 }
 
