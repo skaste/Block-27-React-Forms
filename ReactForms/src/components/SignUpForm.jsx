ecd43809c21b35a4eadc153fd357bb55ea5e059b
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-17 16:09:22 -0600

    got the POST to work and return a token

--- ReactForms/src/components/SignUpForm.jsx
+++ ReactForms/src/components/SignUpForm.jsx
@@ -8,11 +8,22 @@ const SignUpForm = () => {
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log("This is a Test!");
+    // console.log("This is a Test!");
 
-    try{
-
-    }catch (error){
+    try {
+      const response = await fetch(" https://fsa-jwt-practice.herokuapp.com/signup ", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        }),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
       setError(error.message);
     }
   }
@@ -20,6 +31,7 @@ const SignUpForm = () => {
   return (
     <>
       <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Username:{" "}
