7c50956f917b0c51aea3514dbd5fd0a02a088636
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-19 22:28:34 -0600

    working on destructing the token for authenticate.jsx

--- ReactForms/src/App.jsx
+++ ReactForms/src/App.jsx
@@ -3,12 +3,15 @@ import SignUpForm from './components/SignUpForm'
 import Authenticate from './components/Authenticate'
 import './App.css';
 
+
+
 const App = () => {
+  // const [token, setToken] = useState(null);
   return (
     <>
       <h1>Hello World!</h1>
-      <SignUpForm/>
-      <Authenticate/>
+      <SignUpForm /*token={token} setToken={setToken}*//>
+      <Authenticate /*token={token} setToken={setToken}*//>
     </>
   )
 }
